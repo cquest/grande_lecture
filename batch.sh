e797bdd8fbed75897a9738cb21f399e830cd00b3
Christian Quest <cquest@openstreetmap.fr>
2019-03-23 17:02:02 +0100

    batch de génération globale des pdf

---
+++ batch.sh
@@ -0,0 +1,14 @@
+mkdir -p out
+mkdir -p pdf
+cd out
+DEPUTES=$(psql grandelecture -tAc "SELECT replace(nom,' ','_') FROM deputes ORDER BY nom")
+for D in $DEPUTES
+do
+  if [ ! -f ../pdf/$D.pdf ]
+  then
+    python ../data2latex.py "$D" > "$D.tex" \
+    && xelatex "$D.tex" \
+    && xelatex "$D.tex" \
+    && mv "$D.pdf" ../pdf
+  fi
+done
