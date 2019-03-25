cc6dc17b8480133f5a2d419c6e63723a3806d2be
cquest <cquest@openstreetmap.fr>
2019-03-25 12:00:32 +0100

    archivage fichiers LaTeX et génération preview PNG

--- batch.sh
+++ batch.sh
@@ -1,15 +1,24 @@
 mkdir -p out
 mkdir -p pdf
+mkdir -p tex
 cd out
 DEPUTES=$(psql grandelecture -tAc "SELECT replace(nom,' ','_') FROM deputes ORDER BY nom")
 for D in $DEPUTES
 do
   if [ ! -f ../pdf/$D.pdf ]
   then
+    echo "data2latex $D"
     python ../data2latex.py "$D" > "$D.tex" \
     && xelatex "$D.tex" \
     && xelatex "$D.tex" \
     && mv "$D.pdf" ../pdf \
+    && mv "$D.tex" ../tex \
     && rm "$D".*
   fi
+
+  if [ ! -f ../png/$D.png ]
+  then
+    convert ../pdf/$D.pdf[0] -resize 50% ../png/$D.png
+  fi
+
 done
